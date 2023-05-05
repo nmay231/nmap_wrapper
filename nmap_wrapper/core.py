2c86fe98f1de25996b116b1d03ebd9c12a78ffa6
Noah May <noahmouse2011@gmail.com>
2023-05-05 18:50:41 -0500

    Added arbitrary XML walker

--- nmap_wrapper/core.py
+++ nmap_wrapper/core.py
@@ -1,6 +1,7 @@
 import shlex
 import shutil
-from typing import List
+import xml.etree.ElementTree as ET
+from typing import List, TypeVar, Union
 
 nmap_path = shutil.which("nmap")
 
@@ -11,3 +12,31 @@ if not nmap_path:
 def nmap(args: str, xml=True) -> List[str]:
     prefix = "-oX -" if xml else ""
     return shlex.split(f"{nmap_path} {prefix} {args}")
+
+
+T = TypeVar("T")
+
+
+class XMLElement:
+    @staticmethod
+    def fromstring(string: str):
+        return XMLElement(ET.fromstring(string))
+
+    def __init__(self, wrapped: ET.Element):
+        self._wrapped = wrapped
+
+    def __getitem__(self, name: str) -> Union[str, "XMLElement"]:
+        attr = self._wrapped.get(name)
+        if attr is not None:
+            return attr
+        if (child := self._wrapped.find(name)) is not None:
+            return XMLElement(child)
+        raise KeyError(
+            f"`{name}` is not an attribute or child of the `<{self._wrapped.tag}>` tag"
+        )
+
+    def get(self, name: str, alt: T = None) -> Union[str, "XMLElement", T, None]:
+        try:
+            return self[name]
+        except KeyError:
+            return alt
